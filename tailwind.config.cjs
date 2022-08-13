bac57dda24e57c2669ade59ad2226caca8fa746f
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-08-12 22:24:18 -0300

    chore: Initial setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	purge: true,
+  	content: [
+		"./src/**/*.{js,jsx,ts,tsx}"
+  	],
+  	theme: {
+    	extend: {},
+  	},
+  	plugins: [],
+}
