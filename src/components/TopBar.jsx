2358462d124d9681cb803403b15018ab666f780e
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-08-14 15:20:40 -0300

    refactor: Removed topbar animation
    It made the page way too "movy".

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
 	return (
-		<nav className="flex justify-center mt-9 animate-topbar-show-up">
+		<nav className="flex justify-center mt-9">
 			<div className="text-green text-xl md:text-2xl w-1/3">Skills</div>
 			<div className="text-green text-xl md:text-2xl w-1/3">Projects</div>
 			<div className="text-green text-xl md:text-2xl">Get in touch</div>
