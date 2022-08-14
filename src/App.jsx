6a4d306801e7e8d475a58960b820371c21c3cc68
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-08-14 14:32:02 -0300

    chore: Put Presentation section in a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
+import Presentation from './components/Presentation';
+
 export default function App() {
 	return (
 		<>
 			<main>
-				<section>
-					<h1 className="text-6xl md:text-8xl md:ml-16 mt-24 text-center md:text-left font-semibold md:font-normal">
-						Arthur
-						<div className="text-green animate-text-color-change-fast animate-text-wiggle-fast inline-block">
-							D
-						</div>
-						<div className="text-green animate-text-color-change-mid animate-text-wiggle-mid inline-block">
-							e
-						</div>
-						<div className="text-green animate-text-color-change-slow animate-text-wiggle-slow inline-block">
-							v
-						</div>
-					</h1>
-
-					<h3 className="text-2xl text-center md:text-left mx-6 md:mr-0 md:ml-16 mt-8 animate-fade-in-up">
-						I’m a frontend and backend developer.
-					</h3>
-				</section>
+				<Presentation />
 			</main>
 		</>
 	)
